a387ef976f3f96af833671cbebabb52aa6c3bddf
Adarsh Sureshkumar <Adhu.Sury@gmail.com>
2023-12-29 19:04:33 -0500

    added post request

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.model.BasicRequest;
 import com.example.model.BasicResponse;
+import com.example.model.Register;
 
 @CrossOrigin(origins  = {"http://localhost:3000"})
 @SpringBootApplication
@@ -25,10 +27,11 @@ public class DemoApplication {
 		return "Spring boot Hello World - myFirstGet!!! - From Environment :: " + env;
 	}
 	
-	@PostMapping("/myFirstPost")
-	public BasicResponse firstPostMethod(@RequestBody BasicRequest request) {
+	@PostMapping("/register/create")
+	@ResponseBody
+	public BasicResponse firstPostMethod(@RequestBody Register request) {
 		
-		System.out.println("Request: " + request.toString());
+		System.out.println("Register Request: " + request.toString());
 		
 		BasicResponse response = new BasicResponse();
 		response.setCode(0);
---
+++ src/main/java/com/example/model/Register.java
@@ -0,0 +1,43 @@
+package com.example.model;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class Register {
+	
+	private String fname;
+	private String lname;
+	private String email;
+	private String password;
+	
+	
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
+
+}
